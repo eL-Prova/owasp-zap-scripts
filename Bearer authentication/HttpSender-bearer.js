0bf71bf2705c56fac1c0c41513c2601e7130a707
eL-Prova <wimeikelboom100@hotmail.com>
2020-07-17 17:22:54 +0200

    Improve / refactor code
    Refactor HttpSender

--- Bearer authentication/HttpSender-bearer.js
+++ Bearer authentication/HttpSender-bearer.js
@@ -1,13 +1,10 @@
 function sendingRequest(msg, initiator, helper) {
-	//Part of the path for the scope, so not each call will contain the bearer
-	scope_url = "some-part-of-the-url";
-	request_url = msg.getRequestHeader().getURI().toString();
-	if(request_url.indexOf(scope_url) === -1) {
+	if(!msg.isInScope()) {
 		return;
 	}
 
-	var access_token=org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar("access_token");
-	var resource=org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar("resource");
+	var access_token = org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar("access_token");
+	var resource = org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar("resource");
 	
 	//set http header
 	var httpRequestHeader = msg.getRequestHeader();
